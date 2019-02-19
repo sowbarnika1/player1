a0f49e53e5c56956fd22691067c2917bce8507ea
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-19 15:29:50 +0530

    Create set21.c

---
+++ set21.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[20];
+    scanf("%s",a);
+    if(a[0]=='s')
+    {
+        printf("yes");
+    }
+else 
+{
+    printf("no");
+}
+return 0;
+}
