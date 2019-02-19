4e03ba256e184ffb12fe1e4aa3f5a49e9759a669
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-19 15:38:19 +0530

    Create set22.c

---
+++ set22.c
@@ -0,0 +1,20 @@
+int main()
+{
+    int n,k,a[100],i,t;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<k;i++)
+    {
+        t=a[i];
+        a[i]=a[i+1];
+        a[i+1]=t;
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    return 0;
+}
