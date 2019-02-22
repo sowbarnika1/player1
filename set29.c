fbd750c626715f038f2a0df1d0630e463e480cda
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-22 15:42:36 +0530

    Create set29.c

---
+++ set29.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j,flag=0;
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            if(i==2)
+            {
+                printf("%d ",i);
+            }
+            for(j=2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    flag=0;
+                    break;
+                
+                }
+                else
+                {
+                    flag=1;
+                }
+            }
+            if(flag==1)
+            {
+                printf("%d ",i);
+            }
+        }
+        
+    }
+
+    return 0;
+}
