c5b82c7dbd7670486241eed544e0f38da103c28f
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-23 15:19:06 +0530

    Create set38.c

---
+++ set38.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100000],b[100000];
+    int i,l,j=0;
+    gets(a);
+    l=strlen(a);
+    for(i=0;i<l;i++)
+    {
+        if(a[i]!=' ')
+        {
+            b[j]=a[i];
+            j++;
+        }
+    }
+    puts(b);
+    return 0;
+}
