b7d9d88b79ad267f15b7bade587b55b737908916
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-23 22:27:12 +0530

    Create set55.c

---
+++ set55.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int p,a,i,j,per,area,flag=0;
+	scanf("%d %d",&p,&a);
+	for(i=1;i<=(p/2);i++)
+	{
+	    for(j=1;j<=(p/2);j++)
+	{
+	    per=2*(i+j);
+	    area=i*j;
+	    if(per==p && area==a)
+	    {
+	        flag=1;
+	        break;
+	    }
+	}
+	}
+	if(flag==1)
+	{
+	    printf("yes");
+	}
+	if(flag==0)
+	{
+	    printf("no");
+	}
+	
+	return 0;
+}
