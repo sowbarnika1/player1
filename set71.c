ff74e93f5abc387cfe442d2c5c658082edfe5133
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-26 19:13:08 +0530

    Create set71.c

---
+++ set71.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+    int n,x,a[100],i,j,sum,f=0;
+    scanf("%d",&n);
+    scanf("%d",&x);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            sum=a[i]+a[j];
+            if(sum==x)
+            {
+                f=1;
+                break;
+            }
+            else
+            {
+                f=0;
+            }
+        }
+    }
+    if(f==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
