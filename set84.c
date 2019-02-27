3a25d4edcf69f61ba8a12b71ab1c966dacee6108
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-27 19:42:16 +0530

    Update set84.c

--- set84.c
+++ set84.c
@@ -4,7 +4,7 @@ int main()
     long int a,b,i,m=1;
     scanf("%ld",&a);
     scanf("%ld",&b);
-   if((a>=1 && a<=5) && (b>=1 && b<=50))
+   if((a>=1 && a<=50) && (b>=1 && b<=50))
    {
        for(i=1;i<=b;i++)
        {
