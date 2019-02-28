cd6cc39bd6c8a0476816c991d2910c446c573eea
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-02-28 19:49:49 +0530

    Create set89.c

---
+++ set89.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    long int n,a[1000],i,j,d,max;
+    scanf("%ld",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%ld",&a[i]);
+    }
+    max=0;
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            d=a[i]-a[j];
+            if(max>d)
+            {
+                max=d;
+            }
+        }
+    }
+    if(d<0)
+    {
+        max=-max;
+    }
+    else
+    {
+        max=max;
+    }
+        printf("%ld ",max);
+    
+    return 0;
+}
