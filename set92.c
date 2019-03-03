bf98aa3681a21dd905f65234445ded6e3b8fca76
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-03 17:11:24 +0530

    Create set92.c

---
+++ set92.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,a[100],i,j,t;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    if(n==1)
+    {
+        printf("%d",a[0]);
+    }
+    else
+    {
+        t=a[0];
+    for(i=0;i<n-1;i++)
+    {
+        {
+            t=(t&a[i+1]);
+            
+        }
+    }
+    printf("%d",t);
+    }
+    return 0;
+}
