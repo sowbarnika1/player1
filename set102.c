397a7344e7a08e117f68bae403b1480fcbf7eaa6
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-03 20:23:02 +0530

    Create set102.c

---
+++ set102.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    long int a,dec=0,rem,p=1;
+    scanf("%ld",&a);
+    while(a>0)
+    {
+        rem=a%10;
+        dec=dec+rem*p;
+        p=p*2;
+        a=a/10;
+    }
+    printf("%ld",dec);
+    return 0;
+}   
