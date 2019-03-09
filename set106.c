473572084abd075384dd1510746a17be4f9e74f8
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-09 14:11:18 +0530

    Create set106.c

---
+++ set106.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    long int n,t,c=1;
+    int sum,rem,a,b;
+    scanf("%ld",&n);
+    t=n;
+    while(t>0)
+    {
+        t=t/10;
+        c=c*10;
+    }
+    c=c/10;
+        a=n/c;
+        b=n%10;
+        sum=a+b;
+    printf("%d ",sum);
+    return 0;
+}
