3e17cdd19a321b4364e7554e0aa8746350b12ba1
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-10 10:41:53 +0530

    Create set1110.c

---
+++ set1110.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    long int n,a[100000],i,j=0,sum=0,b[100000],l;
+    scanf("%ld",&n);
+    if(n>=1 && n<=100000)
+    {
+        for(i=0;i<n;i++)
+        {
+            scanf("%ld",&a[i]);
+        }
+        for(i=0;i<n;i++)
+        {
+            sum=sum+a[i];
+            b[j]=sum;
+            j++;
+        }
+        sum=0;
+        l=0;
+        for(i=j-1;i>=0;i--)
+        {
+            sum=b[i]+b[l];
+            printf("%ld ",sum);
+            l++;
+        }
+    }
+    return 0;
+}
