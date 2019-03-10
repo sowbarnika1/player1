e516ae507c083c759e3eaaaa79eed9e126852c56
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-10 09:35:03 +0530

    Create set118.c

---
+++ set118.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    long int n,a[100000],i,j,sum=0;
+    scanf("%ld",&n);
+    if(n>=1 && n<=100000)
+    {
+        for(i=0;i<n;i++)
+        {
+            scanf("%ld",&a[i]);
+        }
+        for(i=0;i<n;i++)
+        {
+            sum=sum+a[i];
+            printf("%ld ",sum);
+        }
+    }
+    return 0;
+}
